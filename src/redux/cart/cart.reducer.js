dbc991249dc44b9d867635377435a8f7be226bfe
Liz <admin@elizabethmbank.info>
2020-01-10 10:37:25 -0500

    added addItem and removeItem to checkout component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,7 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
+// import { addItem, removeItem } from './cart.actions';
+
 
 const INITIAL_STATE = {
     hidden: true,
@@ -17,6 +19,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.REMOVE_ITEM:   
+            return {
+               ...state,
+               cartItems: removeItemFromCart(state.cartItems, action.payload) 
             };
         case CartActionTypes.CLEAR_ITEM_FROM_CART:
             return {
