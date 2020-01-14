d5075c9063991ebe746bba438290e4cfd6f452cd
Liz <admin@elizabethmbank.info>
2020-01-14 10:53:47 -0500

    moved shop data and directory into redux folder and created reducers

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,9 @@ import storage from 'redux-persist/lib/storage'; //tells redux you want to use l
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer.js';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
+
 
 const persistConfig = {
     key: 'root',
@@ -13,7 +16,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({ 
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 
 });
 
