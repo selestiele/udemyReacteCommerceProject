40a9b4509b21e4d0b444ff4c3224022f1d35bc2e
Liz <admin@elizabethmbank.info>
2020-01-15 10:40:49 -0500

    added stripe checkout test functionality and updated title and favicon

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -8,6 +8,7 @@ import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector
 
 // import components
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 // import stylesheet
 import './checkout.styles.scss';
@@ -38,6 +39,12 @@ const CheckoutPage = ({cartItems, total}) => (
         <div className="total">
             <span>TOTAL: ${total}</span>
         </div>
+        <div className="test-warning">
+            * Please use the following test credit card for payments* 
+            <br />
+            4242 4242 4242 4242 - Exp 01/21 - CSV 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
